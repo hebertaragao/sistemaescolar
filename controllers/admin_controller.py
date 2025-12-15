881a5ea7df0363be42ad6ec58909820824fbcc24
hebertaragao <hebertaragao.dev@gmail.com>
2025-12-15 10:45:23 -0300

    Update admin_controller.py

---
+++ controllers/admin_controller.py
@@ -0,0 +1,18 @@
+from models.student import Student
+from models.teacher import Teacher
+from models.subject import Subject
+
+def cadastrar_aluno(nome, nascimento, serie):
+    aluno = Student(nome, nascimento, serie)
+    aluno.save()
+    return "Aluno cadastrado com sucesso"
+
+def cadastrar_professor(nome, disciplina):
+    professor = Teacher(nome, disciplina)
+    professor.save()
+    return "Professor cadastrado com sucesso"
+
+def cadastrar_disciplina(nome):
+    disciplina = Subject(nome)
+    disciplina.save()
+    return "Disciplina cadastrada com sucesso"
