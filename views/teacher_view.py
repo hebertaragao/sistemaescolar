52aa5116793bd3eb76f364b4101859fa941cb197
hebertaragao <hebertaragao.dev@gmail.com>
2025-12-15 12:08:07 -0300

    Update teacher_view.py

---
+++ views/teacher_view.py
@@ -0,0 +1,23 @@
+import flet as ft
+from controllers.teacher_controller import lancar_nota, lancar_falta
+
+def teacher_view(page: ft.Page):
+    student_id = ft.TextField(label="ID do Aluno")
+    subject_id = ft.TextField(labek="ID da Disciplina")
+    grade = ft.TextField(label="Nota")
+    msg = ft.Text("")
+
+    def salvar_nota(e):
+        msg.value = lancar_nota(student_id.value, subject_id.value, grade.value)
+        page.update()
+
+    page.add(
+        ft.Column([
+            ft.Text("Painel do Professor", size=20, weight="bold"),
+            student_id,
+            subject_id,
+            grade,
+            ft.ElevatedButton("Lançar Nota", on_click=salvar_nota),
+            msg
+        ])
+    )
